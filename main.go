42b9368aa96664896bcb8b226afa5b50ccf443ef
Branden Colen <BrandenColen@brandens-mbp-2.lan>
2019-12-21 14:42:03 -0600

    cleaned up imports

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 
 	"github.com/brandenc40/insult-generator/handler"
-	"github.com/gin-gonic/gin"
 	"github.com/gorilla/mux"
 )
 
@@ -14,7 +13,6 @@ func main() {
 	insulter := handler.NewInsultGenerator()
 
 	router := mux.NewRouter().StrictSlash(true)
-	r := gin.Default()
 
 	router.HandleFunc("/insult", insulter.GetInsult).Methods("GET")
 	router.HandleFunc("/comeback", insulter.GetComeback).Methods("GET")
